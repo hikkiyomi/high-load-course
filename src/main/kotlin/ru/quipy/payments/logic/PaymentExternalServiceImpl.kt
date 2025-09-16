effb964e8736c2e5d1a13894327d6f634fd2c855
hikkiyomi <hg-4ybak@mail.ru>
2025-09-16 22:35:03 +0300

    feat: rate limit external requests

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -6,6 +6,7 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
 import org.slf4j.LoggerFactory
+import ru.quipy.common.utils.SlidingWindowRateLimiter
 import ru.quipy.core.EventSourcingService
 import ru.quipy.payments.api.PaymentAggregate
 import java.net.SocketTimeoutException
@@ -36,7 +37,18 @@ class PaymentExternalSystemAdapterImpl(
 
     private val client = OkHttpClient.Builder().build()
 
+    private lateinit var rateLimiter: SlidingWindowRateLimiter
+
+    init {
+        val parsedRateLimit = rateLimitPerSec.toLong()
+        val duration = Duration.ofSeconds(1)
+
+        rateLimiter = SlidingWindowRateLimiter(parsedRateLimit, duration)
+    }
+
     override fun performPaymentAsync(paymentId: UUID, amount: Int, paymentStartedAt: Long, deadline: Long) {
+        rateLimiter.tickBlocking()
+
         logger.warn("[$accountName] Submitting payment request for payment $paymentId")
 
         val transactionId = UUID.randomUUID()
