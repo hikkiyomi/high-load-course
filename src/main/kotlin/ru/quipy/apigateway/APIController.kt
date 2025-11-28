7fb351d52990e1851d7144793169dd80c9f66547
hikkiyomi <hg-4ybak@mail.ru>
2025-11-28 19:57:56 +0300

    [case-9] refactor: remove input rate limiters

--- src/main/kotlin/ru/quipy/apigateway/APIController.kt
+++ src/main/kotlin/ru/quipy/apigateway/APIController.kt
@@ -27,27 +27,10 @@ class APIController {
     @Autowired
     private lateinit var paymentMetrics: PaymentMetrics
 
-    private val userRateLimiter = SlidingWindowRateLimiter(
-        1000,
-        Duration.ofSeconds(1),
-    )
-
-    private val orderRateLimiter = SlidingWindowRateLimiter(
-        1000,
-        Duration.ofSeconds(1),
-    )
-
-    private val processRateLimiter = SlidingWindowRateLimiter(
-        1000,
-        Duration.ofSeconds(1),
-    )
-
     private val avgProcessingTime = 10000 // ms
 
     @PostMapping("/users")
     suspend fun createUser(@RequestBody req: CreateUserRequest): ResponseEntity<User> {
-        userRateLimiter.tickSuspend()
-
         return ResponseEntity.ok(User(UUID.randomUUID(), req.name))
     }
 
@@ -57,8 +40,6 @@ class APIController {
 
     @PostMapping("/orders")
     suspend fun createOrder(@RequestParam userId: UUID, @RequestParam price: Int): ResponseEntity<Order> {
-        orderRateLimiter.tickSuspend()
-
         val order = Order(
             UUID.randomUUID(),
             userId,
@@ -86,8 +67,6 @@ class APIController {
 
     @PostMapping("/orders/{orderId}/payment")
     suspend fun payOrder(@PathVariable orderId: UUID, @RequestParam deadline: Long): ResponseEntity<PaymentSubmissionDto> {
-        orderRateLimiter.tickSuspend()
-
         val paymentId = UUID.randomUUID()
 
         val order = orderRepository.findById(orderId)?.let {
